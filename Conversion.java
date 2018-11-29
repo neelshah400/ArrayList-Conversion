0753807175c4f51e860d65c238e9405327239951
anicool20 <45395114+anicool20@users.noreply.github.com>
2018-11-29 10:27:45 -0500

    Update

--- Conversion.java
+++ Conversion.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class Conversion{
 
  	public static void main(String[]args){
-	
+
 		int num = 8950;
 		System.out.println(num + " --> " + convertToArrayList(num));
  	}
- 	
+
  	public static ArrayList<Integer> convertToArrayList(int num){
- 	
+
  		ArrayList<Integer> list = new ArrayList<>();
 		while(num >= 1){
 			list.add(num % 10);
@@ -19,7 +19,9 @@ public class Conversion{
 		for(int i = list.size() - 1; i >= 0; i--)
 			reverse.add(list.get(i));
 		return reverse;
-		
+
  	}
- 	
+ 	public int ReConvert(ArrayList <Integer> list){
+		return 1;
+	}
  }
