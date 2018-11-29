0b9f217287aea69f6b522406c8f7bf5b8549fd09
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-11-29 10:12:57 -0500

    Fix

--- Conversion.java
+++ Conversion.java
@@ -3,20 +3,15 @@ import java.util.ArrayList;
 public class Conversion{
 
 	public static void main(String[]args){
-<<<<<<< HEAD
 
 		//
 		ArrayList <Integer> arr = new ArrayList <>();
 		for(int i = 0; i < 10; i++)
-		ArrayList.add(i);
+		arr.add(i);
 		reConvert(arr);
 
 	}
 	public static int reConvert(ArrayList <Integer> list){
-=======
-	
-		//
-	
->>>>>>> parent of 0be2bc8... Finish convertToArrayList()
+		return 1;
 	}
 }
