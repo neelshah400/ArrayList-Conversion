0be2bc88dca271b3280449db94825d5828178cd0
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-11-29 10:06:13 -0500

    Finish convertToArrayList()

--- Conversion.java
+++ Conversion.java
@@ -3,9 +3,24 @@ import java.util.ArrayList;
 public class Conversion{
 
 	public static void main(String[]args){
-	
-		//
-	
+
+		int num = 8950;
+		System.out.println(num + " --> " + convertToArrayList(num));
+
+	}
+
+	public static ArrayList<Integer> convertToArrayList(int num){
+
+		ArrayList<Integer> list = new ArrayList<>();
+		while(num >= 1){
+			list.add(num % 10);
+			num /= 10;
+		}
+		ArrayList<Integer> reverse = new ArrayList<>();
+		for(int i = list.size() - 1; i >= 0; i--)
+			reverse.add(list.get(i));
+		return reverse;
+
 	}
 
 }
